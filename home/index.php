5918f69bf4c6b687f1899b1ad87af8d7268191b3
Elasania27 <elasania275@gmail.com>
2024-04-02 23:54:16 +0700

    update file modul/home index.php

---
+++ home/index.php
@@ -0,0 +1,4 @@
+<p class="alert alert-info">
+    Selamat datang di aplikasi <strong>Sistem Informasi Akuntansi</strong>,
+    Aplikasi ini dirancang untuk memudahkan manajemen transaksi penjualan
+</p>
