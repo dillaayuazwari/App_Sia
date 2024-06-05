cd5b1428a42ffbcdcf7ce345f6a062e74b7988cc
Elasania27 <elasania275@gmail.com>
2024-06-05 20:44:25 +0700

    update

--- modul/suplier/index.php
+++ modul/suplier/index.php
@@ -1,5 +1,6 @@
 <div class="card mb-3">
     <div class="card-body">
+    <form action="http://localhost/app_sia/modul/suplier/aksi_suplier.php" method="post">
         <div class="row">
             <div class="mb-3 col-md-6">
                 <label for="nama_suplier" class="form-label"> Nama suplier</label>
@@ -22,11 +23,17 @@
         </div>
         <hr>
         <div class="row">
-            <div class="col text-end">
-                <button class="btn btn-secondary" type="reset">Reset</button>
-                <button class="btn btn-primary" type="submit">Simpan</button>
-            </div>
-        </div>
+            <div class="d-flex">
+                <span class="me-auto text-gray">
+                    <?php
+                    if(isset($_SESSION['pesan'])){
+                        echo $_SESSION['pesan'];
+                        unset($_SESSION['pesan']);
+                    }
+                    ?>
+                    </span>
+                    <button type="reset" class="btn btn-secondary">Reset</button>
+                    <button type="submit" name="submit" class="btn btn-primary">Simpan</button>
     </div>
 </div>
 <div class="card">
@@ -47,82 +54,62 @@
                     </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>PT. ABC</td>
-                    <td>Jl. ABC</td>
-                    <td>08123456789</td>
-                    <td>ok5Zt@example.com</td>
-                    <td>
-                        <a href="#editSuplier" class="text-decoration-none" data-bs-toggle="modal">
-                            <i class="bi bi-pencil-square text-success"></i>
-                        </a>
-                        <a href="" class="text-decoration-none">
-                            <i class="bi bi-trash text-danger"></i>
-                        </a>
-                    </td>
-                    <!-- Modal -->
-                    <div class="modal fade" id="editSuplier" tabindex="-1" aria-
-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Edit
-Suplier</h5>
-                                    <button type="button" class="btn-close" data-bs-
-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="mb-3">
-                                        <label class="form-label" for="nama_suplier">Nama
-suplier</label>
-                                        <input type="text" class="form-control"
-name="nama_suplier" value="PT. ABC">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label" for="alamat">Alamat</label>
-                                        <input type="text" class="form-control" name="alamat"
-value="1. ABC">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label" for="telp">Telp</label>
-                                        <input type="text" class="form-control" name="telp"
-value="08123456789">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label" for="email">Email</label>
-                                        <input type="text" class="form-control" name="email"
-value="ok5Zt@example.com">
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-
-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save
-changes</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>PT. XYZ</td>
-                    <td>Jl. XYZ</td>
-                    <td>08123456789</td>
-                    <td>ok5Zt@example.com</td>
-                    <td>
-                        <a href="#editSuplier" class="text-decoration-none"
-data-bs-toggle="modal">
-                            <i class="bi bi-pencil-square text-success"></i>
-                        </a>
-                        <a href="" class="text-decoration-none">
-                            <i class="bi bi-trash text-danger"></i>
-                        </a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+                <?php
+$query = "SELECT * FROM supplier";
+$exec = mysqli_query($koneksi, $query);
+$no = 1;
+while ($data = mysqli_fetch_array($exec)) {
+?>
+<tr>
+    <td><?= $no++ ?></td>
+    <td><?= $data['nama_supplier'] ?></td>
+    <td><?= $data['alamat'] ?></td>
+    <td><?= $data['telp'] ?></td>
+    <td><?= $data['email'] ?></td>
+    <td>
+        <a href="#editSupplier<?= $data['id'] ?>" class="text-decoration-none" data-bs-toggle="modal">
+            <i class="bi bi-pencil-square text-success"></i>
+        </a>
+        <a href="modul/supplier/aksi_supplier.php?act=delete&id=<?= $data['id'] ?>" class="text-decoration-none">
+            <i class="bi bi-trash text-danger"></i>
+        </a>
+    </td>
+</tr>
+<!-- Modal Edit Supplier -->
+<div class="modal fade" id="editSupplier<?= $data['id'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <form action="modul/supplier/aksi_supplier.php?act=update&id=<?= $data['id'] ?>" method="post">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+<h5 class="modal-title" id="exampleModalLabel">Edit Supplier</h5>
+<div class="modal-body">
+    <div class="mb-3">
+        <label class="form-label" for="nama_supplier">Nama Supplier</label>
+        <input type="text" class="form-control" name="nama_supplier" value="<?= $data['nama_supplier'] ?>">
     </div>
+    <div class="mb-3">
+        <label class="form-label" for="alamat">Alamat</label>
+        <input type="text" class="form-control" name="alamat" value="<?= $data['alamat'] ?>">
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="telp">Telp</label>
+        <input type="text" class="form-control" name="telp" value="<?= $data['telp'] ?>">
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="email">Email</label>
+        <input type="text" class="form-control" name="email" value="<?= $data['email'] ?>">
     </div>
 </div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    <button type="submit" class="btn btn-primary">Save changes</button>
+</div>
+</div>
+</div>
+</form>
+</div>
+<?php
+}
+?>
+</tbody>
